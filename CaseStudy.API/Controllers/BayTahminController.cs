5b7efaad799ef719c21672018ee48f1943e57e11
caglar19 <ccakmak19@gmail.com>
2025-01-13 00:08:38 +0300

    BayTahminFirstCommit

---
+++ CaseStudy.API/Controllers/BayTahminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CaseStudy.Application.Models;
+using CaseStudy.Application.Models.Holiday;
+using CaseStudy.Application.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace CaseStudy.API.Controllers
+{
+    [Route("core/api/[controller]/[action]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class BayTahminController : ControllerBase
+    {
+        private readonly IBayTahminService _bayTahminService;
+        private readonly ITokenService _tokenService;
+        private readonly ILogger<BayTahminController> _logger;
+
+
+        public BayTahminController(
+            IBayTahminService bayTahminService,
+            ILogger<BayTahminController> logger)
+        {
+            _bayTahminService = bayTahminService;
+            _logger = logger;
+        }
+
+       
+    }
+}
---
+++ CaseStudy.Application/Services/Impl/BayTahminService.cs
@@ -0,0 +1,11 @@
+﻿using CaseStudy.Application.Models.Holiday;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace CaseStudy.Application.Services.Impl;
+
+public class BayTahminService(HttpClient httpClient) : IBayTahminService
+{
+    
+
+}
